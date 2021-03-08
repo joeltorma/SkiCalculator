a53c47b1c40bea906f639bdcc6743d01c5d3b50d
Joel Törmä <joel.m.torma@gmail.com>
2021-03-08 20:49:53 +0100

    Add ski calculator page

---
+++ SkiCalculator/CalculatorPage/CalculatorInputParams.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SkiCalculator.CalculatorPage
+{
+    public class CalculatorInputParams
+    {
+        public int Length { get; set; }
+        public int Age { get; set; }
+        public string Style { get; set; }
+    }
+}
---
+++ SkiCalculator/CalculatorPage/CalculatorPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkiCalculator.CalculatorPage
+{
+    [Produces("application/json")]
+    [Route("api/CalculatorPage")]
+    public class CalculatorPageController : Controller
+    {
+        [HttpGet("GetSkiLenght")]
+        public int Get([FromBody] CalculatorInputParams inputParams)
+        {
+            var skiLength = inputParams.Age + inputParams.Length;
+            return skiLength;
+        }
+    }
+}
