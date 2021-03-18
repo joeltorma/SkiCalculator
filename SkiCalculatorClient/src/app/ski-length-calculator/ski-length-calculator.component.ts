9458adaa8bc71f8fe7ca643cc2ffaadd7d2944a1
joeltorma <joel.m.torma@gmail.com>
2021-03-18 21:32:18 +0100

    Clean up unnecessary stuff

--- SkiCalculatorClient/src/app/ski-length-calculator/ski-length-calculator.component.ts
+++ SkiCalculatorClient/src/app/ski-length-calculator/ski-length-calculator.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { throwError } from 'rxjs';
 import { __classPrivateFieldSet } from 'tslib';
 import { DataService } from '../data.service';
 
